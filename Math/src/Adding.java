5d3c784923e92129f589f206ee53d433c244e1b8
jonathanko500 <jkko9010@gmail.com>
2017-08-23 11:42:57 -0700

    nothing

---
+++ Math/src/Adding.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Adding {
+	public static void main(String[]args){
+		Addingprocess add = new Addingprocess();
+		System.out.println("Enter 3 numbers to be added.");
+		
+	}
+	
+	
+
+}
