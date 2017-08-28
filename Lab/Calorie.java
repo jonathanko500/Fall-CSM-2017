6074f503f1c4984efd56c36ac1a723ebd41dcaed
jonathanko500 <jkko9010@gmail.com>
2017-08-28 12:32:24 -0700

    nothing

---
+++ Lab/Calorie.java
@@ -0,0 +1,17 @@
+//Jonathan Ko
+//Lab 
+
+
+import java.util.Scnner
+public class Calorie {
+	public static void main(String[] args) {
+		double carbCalories, protienCalories, fatCalories;
+		double gramCarb, gramProtien,gramFat;
+		double fatPct;
+		String foodName;
+		Scanner input = new Scanner(system.in());
+		
+
+	}
+
+}
