305a032e30e76d08500d3201a79cfe68ddfcf723
jonathanko500 <jkko9010@gmail.com>
2017-08-28 11:46:31 -0700

    nothing

---
+++ Intro/src/HelloWorld.java
@@ -0,0 +1,12 @@
+//Jonathan Ko 
+//Lab 3 problem 1
+//Prints Hello world
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Kennedy Louie neeeds to die");
+
+	}
+
+}
